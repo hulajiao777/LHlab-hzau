1d5592a6cd1672c7e6a926f22a92dfb1bb1580dc
Liuhai626 <2482153088@qq.com>
2025-05-21 14:50:44 +0800

    emmm

--- components/fetch-md.ts
+++ components/fetch-md.ts
@@ -1,5 +1,5 @@
 //http://localhost:3000/md/contact.md
 
 export function getMd(cat:string){
-    return fetch(`http://localhost:3000/md/${cat}.md`).then(res=>res.text()).then(data=>data)
+    return fetch(`https://l-hlab-hzau-git-main-liuhais-projects.vercel.app/md/${cat}.md`).then(res=>res.text()).then(data=>data)
 }
