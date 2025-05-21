55dcc775d673aa9f1096fe3c1963e242a8e8e5f8
Liuhai626 <2482153088@qq.com>
2025-05-21 14:48:29 +0800

    emmm

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/my-nextjs-app' : '',
-  images: {
-    unoptimized: true
-  }
 };
 
 export default nextConfig;
